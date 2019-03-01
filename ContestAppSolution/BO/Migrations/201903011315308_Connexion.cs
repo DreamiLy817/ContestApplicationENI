4765fcbaa24f5bc27b51dd28e6f7dbc30cc4eba7
Alex <a@a.com>
2019-03-01 14:42:35 +0100

    Création des bases utilisateur

---
+++ ContestAppSolution/BO/Migrations/201903011315308_Connexion.cs
@@ -0,0 +1,16 @@
+namespace BO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Connexion : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
