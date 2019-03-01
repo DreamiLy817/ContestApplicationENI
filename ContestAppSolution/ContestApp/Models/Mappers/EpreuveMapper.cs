948d9d45871cf7cfc8e4fec890b1caa68057ae20
Milho <labulledemilho@gmail.com>
2019-03-01 14:27:13 +0100

    EpreuveMapper init

---
+++ ContestAppSolution/ContestApp/Models/Mappers/EpreuveMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContestApp.Models.Mappers
+{
+    public static class EpreuveMapper
+    {
+        public static EpreuveViewModel Map()
+        {
+            return new EpreuveViewModel
+            {
+                
+            };
+
+        }
+    }
+}
