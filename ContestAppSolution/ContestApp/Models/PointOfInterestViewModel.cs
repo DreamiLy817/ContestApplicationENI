ed6dfeb1a2bbd1be342785d3d84daea2e4780c09
Milho <labulledemilho@gmail.com>
2019-03-01 12:09:42 +0100

    Création ViewModels

---
+++ ContestAppSolution/ContestApp/Models/PointOfInterestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContestApp.Models
+{
+    public class PointOfInterestViewModel
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public decimal Distance { get; set; }
+        public CategorieViewModel Categorie { get; set; }
+        public EpreuveViewModel Epreuve { get; set; }
+    }
+}
